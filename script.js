3afb5adb286ca347ffca7e2f180457fb193a721f
laurelr04 <laurel@laurelham.com>
2013-06-06 17:30:22 -0300

    Portfolio JS
    Javascript used for my portfolio functionality

---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  // gray out images on hover
+	$(".portfolio a").hover( function(){ 
+		$(this).children("img").animate({ opacity: 0.75 }, "fast"); 
+	}, function(){ 
+		$(this).children("img").animate({ opacity: 1.0 }, "slow"); 
+	}); 
+	
+	// Clone portfolio items to get a second collection for Quicksand plugin
+	var $portfolioClone = $(".portfolio").clone();
+	
+	// Attempt to call Quicksand on every click event handler
+	$(".filter a").click(function(e){
+		
+		$(".filter li").removeClass("current");	
+		
+		// Get the class attribute value of the clicked link
+		var $filterClass = $(this).parent().attr("class");
+
+		if ( $filterClass == "all" ) {
+			var $filteredPortfolio = $portfolioClone.find("li");
+		} else {
+			var $filteredPortfolio = $portfolioClone.find("li[data-type~=" + $filterClass + "]");
+		}
+		
+		// Call quicksand
+		$(".portfolio").quicksand( $filteredPortfolio, { 
+			duration: 800, 
+			easing: 'easeInOutQuad' 
+		}, function(){
+			
+			// Blur newly cloned portfolio items on mouse over and apply prettyPhoto
+			$(".portfolio a").hover( function(){ 
+				$(this).children("img").animate({ opacity: 0.5 }, "fast"); 
+			}, function(){ 
+				$(this).children("img").animate({ opacity: 1.0 }, "slow"); 
+			}); 
+		});
+
+
+		$(this).parent().addClass("current");
+
+		// Prevent the browser jump to the link anchor
+		e.preventDefault();
+	})
+});
